cf471de770bd607359e4e40f9669329b59fd4dc8
Neriman Dikmen <sanerneriman@gmail.com>
2024-02-01 18:10:44 -0500

    add post

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [input, setInput] = useState("");
+  const [message, setMessage] = useState("")
+
+  const handleOnChange = (e) => {
+    setInput(e.target.value)
+  }
+
+  const onSubmit = () => {
+    fetch('https://heroku-test-neri-6c75de1160cd.herokuapp.com/api/deneme-submission', {
+      method: 'POST',
+      body: JSON.stringify({ message: input }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setMessage(data.message);
+      })
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +39,10 @@ function App() {
         >
           Learn React
         </a>
+        <input type="text" onChange={handleOnChange} value={input}/>
+        <button onClick={onSubmit}>Submit</button>
+        <h2>MESSAGE</h2>
+        {message}
       </header>
     </div>
   );
--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const PORT = process.env.PORT || 8001;
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
+const bodyparser = require("body-parser");
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "/client/build")));
+
+
+app.post('/api/deneme-submission', (req, res) => {
+  const incomingMessage = req.body.message;
+  if (incomingMessage) {
+    res.json({
+      status: "ok",
+      message: `Message received: Here is your the message: ${incomingMessage}`
+    })
+  }
+})
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
 });
+
+// asda sd
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}.`);
